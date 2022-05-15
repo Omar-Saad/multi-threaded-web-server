254a9226c724e7f6cc0dedd7b41d20613b4d44fb
Omar Saad <62719848+Omar-Saad@users.noreply.github.com>
2022-05-15 05:32:55 +0200

    HTTP 1.1 : support persistent connection and pipeline

--- client/client.py
+++ client/client.py
@@ -1,3 +1,4 @@
+from utils.parser import *
 from http import client
 import socket
 import sys
@@ -6,42 +7,49 @@ from os.path import exists as file_exists
 
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
- 
-from utils.parser import *
 
 
 # constants
 MSG_SIZE = 1024
 FORMAT = "utf-8"
 
+
 class Client:
-    def __init__(self,command):
-        self.request,port_number = self.__createHTTPRequestFromCommand(command)
+    def __init__(self, command):
+        # creating HTTP request from the received command
+        self.request, port_number = self.__createHTTPRequestFromCommand(
+            command)
+        # parsing HTTP request
         self.client_request = RequestParser(self.request)
 
+        # inititing Server IP and port
         self.SERVER_IP = socket.gethostbyname(self.client_request.host_name)
         self.SERVER_PORT = port_number
-        self.ADDR = (self.SERVER_IP,self.SERVER_PORT)
+        self.ADDR = (self.SERVER_IP, self.SERVER_PORT)
 
         self.client = None
         try:
+            # connecting with the server
             self.client = self.__connect()
         except Exception as e:
             print("ERROR : WRONG HOST NAME OR PORT NUMBER")
 
-
     def sendHTTPRequest(self):
+        '''
+        this fuction sends an HTTP request 
+        GET and POST
+        '''
         if self.client == None:
             print("ERROR : COULD NOT CONNECT TO SERVER")
             return
 
         if self.client_request.method == "POST":
-        
+
             request_to_send = self.request
-            
+
             # read file data
             if file_exists(self.client_request.file_name):
-                file = open(self.client_request.file_name,"r")
+                file = open(self.client_request.file_name, "r")
                 data = file.read()
 
                 request_to_send += data
@@ -51,7 +59,6 @@ class Client:
                 print("ERROR IN Client > File not found in client directory")
                 self.client.close()
                 return
-                
 
             # upload file data to server
             self.client.send(request_to_send.encode(FORMAT))
@@ -61,8 +68,8 @@ class Client:
             print(received_msg)
 
             self.client.close()
-        
-        elif self.client_request.method == "GET" :
+
+        elif self.client_request.method == "GET":
             self.client.send(self.request.encode(FORMAT))
 
             received_msg = self.client.recv(MSG_SIZE).decode(FORMAT)
@@ -71,24 +78,28 @@ class Client:
 
             response_parser = ResponseParser(received_msg)
 
-            if response_parser.status == 200 and response_parser.data!=None:
-            #  download file in client directory
+            if response_parser.status == 200 and response_parser.data != None:
+                #  download file in client directory
                 file_name = self.client_request.file_name.split("/")[-1]
-                file = open(file_name,"w")
+                file = open(file_name, "w")
                 file.write(response_parser.data)
                 file.close()
 
             self.client.close()
 
-    
     def __connect(self):
-        client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        # initiating socket
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         client.connect(self.ADDR)
-        print("client is connected to IP:",self.SERVER_IP,"PORT:",self.SERVER_PORT)
+        print("client is connected to IP:",
+              self.SERVER_IP, "PORT:", self.SERVER_PORT)
         return client
 
-    def __createHTTPRequestFromCommand(self,command,httpVersion= 1.1):
+    def __createHTTPRequestFromCommand(self, command, httpVersion=1.1):
+        '''
+        this function creates simple HTTP request from command
+        '''
         request_lines = command.split(" ")
         method = request_lines[0]
         file_name = request_lines[1]
@@ -97,30 +108,36 @@ class Client:
             port_number = int(request_lines[3])
         else:
             port_number = 80
-        request = method+" "+file_name +" HTTP/"+str(httpVersion)+"\r\nHost:"+host_name+":"+str(port_number)+"\r\n\r\n"
-        return request,port_number
-
+        request = method+" "+file_name + " HTTP/" + \
+            str(httpVersion)+"\r\nHost:"+host_name + \
+            ":"+str(port_number)+"\r\n\r\n"
+        return request, port_number
 
 
 # START OF CLIENT
-if __name__=="__main__":
-    
+if __name__ == "__main__":
+
     commands = []
+    # check if input file name is given as argument
     if len(sys.argv) == 2:
+        # read all commands from the input file
         file_name = sys.argv[1]
-        file = open(file_name,"r")
+        file = open(file_name, "r")
         data = file.read()
         # list of commands in input file
         commands = data.split("\n")
         file.close()
-        
+
     else:
+        # take command from input terminal
         command = input(">")
         commands.append(command)
 
     print("CLIENT STARTED")
 
+    # loop over commands
     for command in commands:
+        # create a client request for each command
         client = Client(command.strip())
         client.sendHTTPRequest()
         print("\n")
@@ -128,6 +145,3 @@ if __name__=="__main__":
         print("\n")
 
     print("CLIENT CLOSED")
-
-
-    
--- server/server.py
+++ server/server.py
@@ -1,15 +1,17 @@
+from utils.parser import RequestParser
+from os.path import exists as file_exists
 import socket
 import threading
 import sys
 import os.path
+from time import sleep
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
-from os.path import exists as file_exists
- 
-from utils.parser import RequestParser
 
 # IP = socket.gethostbyname(socket.gethostname())
+
+# default port number
 PORT = 80
 # ADDR = (IP,PORT)
 MSG_SIZE = 1024
@@ -22,99 +24,163 @@ NOT_FOUND_RESPONSE = "HTTP/1.1 404 Not Found\r\n"
 
 
 class Server:
-    def __init__(self,port_number = PORT):
-        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    def __init__(self, port_number=PORT):
+        # inititing server  socket
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # IP = socket.gethostbyname(socket.gethostname())
         IP = "127.0.0.1"
-        ADDR = (IP,port_number)
+        ADDR = (IP, port_number)
+        # self.server.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
 
         self.server.bind(ADDR)
+
         self.server.listen()
-        print("DEBUG : server is listening on IP:",IP,"PORT:",PORT)
+        print("DEBUG : server is listening on IP:", IP, "PORT:", PORT)
 
     def start_server(self):
+
         while True:
-        
-            conn,addr = self.server.accept()
-            
-            thread = threading.Thread(target=self.handle_client,args=(conn,addr))
+            # waiting for client to connect
+            conn, addr = self.server.accept()
+
+            # creating new thread for each client
+            thread = threading.Thread(
+                target=self.handle_client, args=(conn, addr))
+
+            # starting thread  : will call handle_client() function
             thread.start()
+
             # show number of active threads (connections)
-            print("DEBUG : Number of active connections (threads)",(threading.active_count()-1))
-    
+            # print("DEBUG : Number of active connections (threads)",(threading.active_count()-1))
 
-    
+    def handle_client(self, conn, addr):
+        '''
+        this function handles all client requests
+        and creates a new thread for each client request (pipeline)
+        the connection is closed after certain time delay (timeout)
+        '''
 
-    def handle_client(self,conn,addr):
-        print("DEBUG : Client connected from:",addr)
-        
-        msg = conn.recv(MSG_SIZE).decode(FORMAT)
-        print(addr,">",msg)
-        if msg != "":
-            client_request = RequestParser(msg)
-        else:
+        print("DEBUG : Client connected from:", addr)
+
+        # setting timeout for client connection
+        TIME_OUT_SEC = 15
+        conn.settimeout(TIME_OUT_SEC)
+        while True:
+            try:
+                # receiving client request
+                msg = conn.recv(MSG_SIZE).decode(FORMAT)
+
+                print(addr, ">", msg)
+
+                # check if the received message is not empty
+                # empty message means that client closed the connection
+                if msg != "":
+                    # parsing client request
+                    client_request = RequestParser(msg)
+                    # creating new thread to handle client request
+                    thread = threading.Thread(
+                        target=self.handle_client_request, args=(conn, addr, client_request))
+                    thread.start()
+
+                else:
+                    # empty message means that client closed the connection
+                    print(
+                        "DEBUG EMPTY MESSAGE RECEIVED: Client disconnected from:", addr)
+                    conn.close()
+                    return
+
+            except socket.timeout:
+                # timeout means that client did not send any message
+                print("DEBUG TIMEOUT : Server closes connection with :", addr)
+                # close the connection with client
+                conn.close()
+                return
+            except Exception as e:
+                print("DEBUG ERROR : Server closes connection with :", addr)
+                conn.close()
+                return
+
+    def handle_client_request(self, conn, addr, client_request):
+        ''''
+        this function called by handle_client() 
+        and is called for each client request for the same connection
+
+        '''
+        try:
+
+            if client_request.method == POST_REQUEST:
+                self.handle_post_request(conn, addr, client_request)
+
+            elif client_request.method == GET_REQUEST:
+                self.handle_get_request(conn, addr, client_request)
+
+        except socket.timeout:
+            # timeout means that client did not send any message
+
+            print("DEBUG TIMEOUT : Server closes connection with :", addr)
+            # m = "HTTP/1.1 408 Request Timeout\r\n\r\n"
+            # conn.send(m.encode(FORMAT))
+            # close the connection with client
             conn.close()
             return
-        
-        if client_request.method == POST_REQUEST:
-            self.handle_post_request(conn,addr,client_request)
-            print("DEBUG : Client disconnected from:",addr)
+        except Exception as e:
+            print("DEBUG ERROR : Server closes connection with :", addr)
+            conn.close()
             return
 
-        elif client_request.method == GET_REQUEST:
-            self.handle_get_request(conn,addr,client_request)
-            print("DEBUG : Client disconnected from:",addr)
-            return
+    def handle_post_request(self, conn, addr, client_request):
+        '''
+        this function handles POST request
+        '''
 
-
-    def handle_post_request(self,conn,addr,client_request):
-
-        conn.send(OK_RESPONSE.encode(FORMAT))
-
-        # data = conn.recv(MSG_SIZE).decode(FORMAT)
+        msg = OK_RESPONSE+"\r\n"
+        conn.send(msg.encode(FORMAT))
 
         filename = client_request.file_name
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        # check if the file name is a directory or not
+
+        if os.path.isdir(filename):
+            # if directory then creates new directory
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        # creates new file in the specified directory
         with open(filename, "w") as f:
+            # write all received data from the client to the file
             f.write(client_request.data)
 
-        # file_name = client_request.file_name.split("/")[-1]
-        # file = open(file_name,"w")
-        
-        # file.write(client_request.data)
-        # file.close()
-
-        conn.close()
         print()
-        print(addr,"> ",client_request.data)
-    
-        
-    def handle_get_request(self,conn,addr,client_request):
-        
+        print(addr, "> ", client_request.data)
+
+    def handle_get_request(self, conn, addr, client_request):
+        '''
+        this function handles GET request
+        '''
+
+        # check if the file not exists
         if not file_exists(client_request.file_name):
+            # if file not exists then send 404 error
             print("DEBUG: file not found")
             msg = NOT_FOUND_RESPONSE + "\r\n"
             conn.send(msg.encode(FORMAT))
-            conn.close()
+
             return
         else:
+            # if file exists then send 200 OK and file content
 
             msg = OK_RESPONSE+"\r\n"
-            print(client_request.file_name)
-            file = open(client_request.file_name,"r")
+
+            file = open(client_request.file_name, "r")
             data = file.read()
             msg = msg+data+"\r\n"
-    
+
             conn.send(msg.encode(FORMAT))
-            
+
             file.close()
-            conn.close()
-  
-            
-       
+
+        return
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print("DEBUG : SERVER STARTED")
     server = Server()
     server.start_server()
